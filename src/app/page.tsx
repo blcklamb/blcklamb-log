3a3deb8c16df2491c83a6b6178d4aba28dea0afb
blcklamb <whenucan35@gmail.com>
2024-02-02 18:33:04 +0900

    💫 move to postList from main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Icon from "@/components/Icon";
 import IconLoader from "@/components/IconLoader";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,15 +13,16 @@ export default function Home() {
             <code className="font-mono font-bold">src/app/page.tsx</code>
           </p>
         </div>
-
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-          <Icon
-            name="next"
-            width={180}
-            height={37}
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          />
-        </div>
+        <Link href="/posts/all">
+          <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+            <Icon
+              name="next"
+              width={180}
+              height={37}
+              className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+            />
+          </div>
+        </Link>
 
         <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
           <a
