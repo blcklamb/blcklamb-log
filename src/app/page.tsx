0ec654d1f2a262cc3bc39463d490f6ad06adf7ee
blcklamb <whenucan35@gmail.com>
2024-12-06 22:23:35 +0900

    💄 floating sheep

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import LandingOverlay from "@/components/LandingOverlay";
+import FloatingSheep from "@/components/LandingSample";
+import { Suspense } from "react";
+
 export default function Home() {
-  return <>Hello World</>;
+  return (
+    <div>
+      <Suspense fallback={null}>
+        <FloatingSheep />
+      </Suspense>
+      <LandingOverlay />
+    </div>
+  );
 }
