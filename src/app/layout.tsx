5b02a62f712064b029c70d811260ecd71c17f072
blcklamb <whenucan35@gmail.com>
2026-07-07 00:13:57 +0900

    💄 add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,32 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://blog.blcklamb.net"),
   title: "blog blcklamb | 김채정",
   description: "프론트엔드 기술 학습 및 공유를 합니다.",
   icons: { icon: "/icon.ico" },
+  openGraph: {
+    title: "blog.blcklamb",
+    description: "프론트엔드 기술 학습 및 공유를 합니다.",
+    url: "/",
+    siteName: "blog.blcklamb",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "blog.blcklamb",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "blog.blcklamb",
+    description: "프론트엔드 기술 학습 및 공유를 합니다.",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
